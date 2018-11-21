cf6c00331a27e5e06f8d55136b2b8f9feffcc743
jinnjuu <jjinju222@gmail.com>
2018-11-21 22:37:45 +0900

    add function

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,8 +1,20 @@
-
+import java.util.*;
 public class Contacts {
-	
+	static Scanner scan=new Scanner(System.in);
 	public static void contactmenu() {
 		PersonalInfo pc = new PersonalInfo();
 		pc.printDetailMenu();
+	}
+	public static void createContact() {
+		//create contact
+	}
+	public void viewContact() {
+		//view contact
+	}
+	public static void updateContact() {
+		//update contact
+	}
+	public static void deleteContact() {
+		//delete contact
 	}
 }
