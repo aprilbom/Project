ac246948d9dbad7acd29eeafb4c0ab0d5b4ffaaa
aprilbom <44661703+aprilbom@users.noreply.github.com>
2018-11-21 15:01:03 +0900

    added delete, view, update method

--- src/PersonalInfo.java
+++ src/PersonalInfo.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class PersonalInfo {
 	public static void printMainMenu() {
-		System.out.println("<Main Menu>\n1. Contact\n2. ToDoList\n3. Appointment\n4. Note\n5. Exit");
+		System.out.println("\n<Main Menu>\n1. Contact\n2. ToDoList\n3. Appointment\n4. Note\n5. Exit");
 		System.out.print("Enter Number: ");
 	}
 	
@@ -17,7 +17,7 @@ public class PersonalInfo {
 		// 클래스 객체 생성
 		ToDoList todo = new ToDoList();
 		
-		System.out.println("Hello!\n");
+		System.out.println("Hello!");
 		printMainMenu();
 		
 		Scanner scan = new Scanner(System.in);
@@ -28,6 +28,7 @@ public class PersonalInfo {
 			else if (n == 2) todo.toDoListMenu();
 			else if (n == 3) ;
 			else if (n == 4) ;
+			printMainMenu();
 		}
 		scan.close();
 	}
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -6,6 +6,7 @@ public class ToDoList {
 	static Vector<todo> v = new Vector<todo>();
 	
 	static class todo implements Serializable {
+
 		private String createdate;
 		private String duedate;
 		private String description;
@@ -41,9 +42,28 @@ public class ToDoList {
 		}
 	}
 	
+	private static void saveFile() throws Exception {
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("To Do List.dat"));
+		oos.writeObject(v);
+		oos.close();
+	}
+	
 	public static void toDoListMenu() throws Exception {
+		// 프로그램 실행될때마다 파일 있는지 확인
+		// 확인해서 파일 없으면 새로 만들어서 빈 백터 넣기
+		// 확인해서 파일 있으면 파일 내용 새 백터에 읽어오기 
+		
+		File f = new File("To Do List.dat");
+		if (!f.exists()) { // 파일 없으면
+			f.createNewFile();
+		} else { // 파일 있으면
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+			v = (Vector<todo>)ois.readObject();
+			ois.close();
+		}
+		
 		PersonalInfo pInfo = new PersonalInfo();
-		System.out.println("<To Do List Menu>");
+		System.out.println("\n<To Do List Menu>");
 		pInfo.printDetailMenu();
 		
 		Scanner scan = new Scanner(System.in);
@@ -51,14 +71,15 @@ public class ToDoList {
 		int n = scan.nextInt();
 		
 		while (n != 5) {
-			if (n == 1) {
-				createToDoList();
-				fileStream();
-			}
+			if (n == 1) createToDoList();
 			else if (n == 2) viewToDoList();
-			else if (n == 3) deleteToDoList();
-			else if (n == 4) updateToDoList();
-			System.out.println("<To Do List Menu>");
+			else if (n == 3) updateToDoList();
+			else if (n == 4) deleteToDoList();
+			else if (n == 5) {
+				break;
+			}
+			saveFile();
+			System.out.println("\n<To Do List Menu>");
 			pInfo.printDetailMenu();
 			n = scan.nextInt();
 		}
@@ -79,85 +100,49 @@ public class ToDoList {
 		
 		v.add(new todo(createdate, duedate, description));
 	}
-	
-	private static void fileStream() throws Exception {
-		File f = new File("ToDoList.dat");
-		if (!f.exists()) {
-			FileOutputStream fout = null;
-			ObjectOutputStream oos = null;
-			try {
-				fout = new FileOutputStream("ToDoList.dat");
-				oos = new ObjectOutputStream(fout);
-				oos.writeObject(v);
-				oos.reset();
-			} catch(Exception e) {
-				System.out.println(e);
-			} finally {
-				try {
-					fout.close();
-					oos.close();
-				} catch (IOException ie) {
-					System.out.println(ie);
-				}
-			}
-		} else {
-			FileInputStream fin = null;
-			ObjectInputStream ois = null;
-			try {
-				fin = new FileInputStream("ToDoList.dat");
-				ois = new ObjectInputStream(fin);
-				Vector<todo> inputv = (Vector<todo>)ois.readObject();
-				v.addAll(inputv);
-			} catch(Exception e) {
-				System.out.println(e);
-			} finally {
-				try {
-					fin.close();
-					ois.close();
-				} catch (IOException ie) {
-					System.out.println(ie);
-				}
-			}
-		}
-	}
-	
+
 	public static void viewToDoList() throws Exception{
-		Vector<todo> viewv = new Vector<todo>();
-		/*FileInputStream fin = null;
-		ObjectInputStream ois = null;
-		try {
-			fin = new FileInputStream("ToDoList.dat");
-			ois = new ObjectInputStream(fin);
-			viewmap = (HashMap<String, todo>)ois.readObject();
-		} catch(Exception e) {
-			System.out.println(e);
-		} finally {
-			try {
-				fin.close();
-				ois.close();
-			} catch (IOException ie) {
-				System.out.println(ie);
-			}
-		}*/
-		
-		int n = 1;
-		Iterator<todo> iter = viewv.iterator();
-		while(iter.hasNext()) {
-			todo temp = iter.next();
-			System.out.println("---------- " + n + " ----------");
-			System.out.println("Create Date: " + temp.createdate);
-			System.out.println("Due Date: " + temp.duedate);
-			System.out.println("Description: " + temp.description);
+		System.out.println(" ");
+		for (int i = 0; i < v.size(); i++) {
+			System.out.println("---------- " + (i + 1) + " ----------");
+			System.out.println("Create Date: " + v.get(i).createdate);
+			System.out.println("Due Date: " + v.get(i).duedate);
+			System.out.println("Description: " + v.get(i).description);
 			System.out.println("------------------------");
-			n++;
 		}
 	}
 	
-	public static void deleteToDoList() {
-		
+	// 수정해야함
+	public static void deleteToDoList() throws Exception{
+		viewToDoList();
+		System.out.print("\nEnter To Do List Number you want to delete: ");
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		v.remove(n - 1);
+		System.out.println("Complete!");
 	}
 	
-	public static void updateToDoList() {
+	public static void updateToDoList() throws Exception{
+		viewToDoList();
+		System.out.print("\nEnter To Do List Number you want to update: ");
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
 		
+		System.out.print("Enter updated Duedate(YYMMDD): ");
+		String duedate = scan.next();
+		
+		// 현재 날짜 받아오기
+		SimpleDateFormat format = new SimpleDateFormat("yyMMdd");
+		Calendar c1 = Calendar.getInstance();
+		String createdate = format.format(c1.getTime());
+		
+		System.out.print("Enter updated Description: ");
+		String description = scan.next();
+		
+		v.get(n).createdate = createdate;
+		v.get(n).duedate = duedate;
+		v.get(n).description = description;
+		
+		System.out.println("Complete!");
 	}
 }
