36b8a501acc81c61b3c533e784e98eb3aa0b7551
jinnjuu <jjinju222@gmail.com>
2018-11-22 17:15:22 +0900

    modify contactmenu

--- src/Contacts.java
+++ src/Contacts.java
@@ -3,12 +3,30 @@ public class Contacts {
 	static Scanner scan=new Scanner(System.in);
 	public static void contactmenu() {
 		PersonalInfo pc = new PersonalInfo();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("\n<Contacts Menu>");
 		pc.printDetailMenu();
+		int n = scan.nextInt();
+		
+		while (n != 5) {
+			if (n == 1) createContact();
+			else if (n == 2) viewContact();
+			else if (n == 3) updateContact();
+			else if (n == 4) deleteContact();
+			else if (n == 5) {
+				return;
+			}
+			//saveFile();
+			System.out.println("\n<Contacts Menu>");
+			pc.printDetailMenu();
+			n = scan.nextInt();
+		}
 	}
 	public static void createContact() {
 		//create contact
 	}
-	public void viewContact() {
+	public static void viewContact() {
 		//view contact
 	}
 	public static void updateContact() {
